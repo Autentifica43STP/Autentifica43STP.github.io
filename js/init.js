32fc7bd505515fb7bbc439824f803e218984bb7b
Autentifica43STP <81206546+Autentifica43STP@users.noreply.github.com>
2021-03-22 21:20:46 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,11 @@
-
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCvmjzVAeqs0xmdYptc_LlrunPODy5SDQk",
+    authDomain: "autentifica43tps.firebaseapp.com",
+    projectId: "autentifica43tps",
+    storageBucket: "autentifica43tps.appspot.com",
+    messagingSenderId: "843320142991",
+    appId: "1:843320142991:web:fbc25412198f14d72154fa"
+    });
